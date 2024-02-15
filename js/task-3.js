49bf1747b6ad532a57430ec8490f09ea5a9119aa
Oleg <zazubik78@gmail.com>
2024-02-15 16:50:48 +0200

    hw03 ready

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+  const arr = [];
+  for (let i = 0; i < numbers.length; i++){
+    if (numbers[i] > value) {
+      arr.push(numbers[i]);
+    }
+  }
+  return arr;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
